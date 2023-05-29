023857ae66f9a9c080b7daa68adb7b9f9df8370f
Sakshi Agrawal <128794880+Saki-4148@users.noreply.github.com>
2023-05-29 22:47:41 +0530

    Add files via upload

---
+++ select.js
@@ -0,0 +1,17 @@
+const anime=document.getElementById("anime");
+const books=document.getElementById("books");
+const capital=document.getElementById("capital");
+const sn=document.getElementById("sn");
+
+anime.addEventListener("click",()=>{
+    window.location.assign("Quizzing website1.html");
+})
+books.addEventListener("click",()=>{
+    window.location.assign("Quizzing website2.html");
+})
+capital.addEventListener("click",()=>{
+    window.location.assign("Quizzing website3.html");
+})
+sn.addEventListener("click",()=>{
+    window.location.assign("Quizzing website4.html");
+})
